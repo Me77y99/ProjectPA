6ff2f91072fad4434fa7926ced4082f3ac481d64
Francesco <30899725+Franz95@users.noreply.github.com>
2022-06-21 20:21:59 +0200

    Commit repair

---
+++ App/model/Recipe_foods.ts
@@ -0,0 +1,29 @@
+import { ConnectionDb } from '../connection';
+import { DataTypes, Sequelize } from 'sequelize';
+
+const sequelize: Sequelize = ConnectionDb.getInstanceConnection();
+
+export const Recipe_foods = sequelize.define('Recipe_foods', {
+    recipe_id: {
+        type: DataTypes.INTEGER(),
+        allowNull: false
+    },
+    food_id: {
+        type: DataTypes.INTEGER(),
+        allowNull: false
+    },
+    sort: {
+        type: DataTypes.INTEGER(),
+        allowNull: false
+    },
+    rate: {
+        type: DataTypes.INTEGER(),
+        allowNull: false
+    }
+}, 
+{
+    modelName: 'Recipe_foods',
+    timestamps: false
+});
+
+Recipe_foods.removeAttribute('id');
